67004ee28b286c454c665cf4391560dd67a3a519
kroxa <tchernencko.ver@yandex.ru>
2022-01-22 19:04:02 +0300

    i did it

---
+++ main.py
@@ -0,0 +1,46 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QPushButton
+from random import randrange
+
+
+class Board(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(400, 400, 800, 600)
+        self.setWindowTitle('Желтые окружности')
+        self.pushButton.clicked.connect(self.draw_yellow_circle)
+        self.not_draw = True
+
+    def draw_yellow_circle(self):
+        self.not_draw = False
+        self.repaint()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawFlag(qp)
+        qp.end()
+
+    def drawFlag(self, qp):
+        if not self.not_draw:
+            x = randrange(0, 400)
+            y = randrange(0, 400)
+            radius = randrange(5, 200)
+            qp.setBrush(QColor('yellow'))
+            qp.drawEllipse(x, y, radius, radius)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Board()
+    ex.show()
+    sys.exit(app.exec_())
+
